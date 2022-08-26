88d3c896fe0b1a98df5888755c5feded63caca4b
henridetroost <95912098+henridetroost@users.noreply.github.com>
2022-08-26 20:21:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Credentials:
-    app_id = os.environ['APP_ID']
-    app_secret = os.environ['APP_SECRET']
+    app_id = os.environ['63090ee0d8025698d0be8ef5']
+    app_secret = os.environ['beb4eb567211913a039c79016ab5c44ac3ec71e267a3c00e994f1d57de2b25e8']
